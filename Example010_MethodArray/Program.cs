cc8726d6a44b03cf02d96a3849aea7e6ee63d28a
Sergei Rostov <sergeitoday@gmail.com>
2022-06-01 01:58:41 +0400

    Add Examples008-011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 2, 7, 4, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
+
